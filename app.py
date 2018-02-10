c6667e79f0394f8affbfcf13d2ed4d6509cbe9ad
Bobbie Chen <bobbiechen75@gmail.com>
2018-02-09 23:15:17 -0500

    Db boilerblate

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
@@ -15,7 +19,6 @@ def report_flush():
 
     # Todo: write to the database
     return "Post success", 200
-
 
 if __name__ == '__main__':
     app.run()
