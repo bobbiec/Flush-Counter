79bd4de5ae7756e2264fd85cf22b0a74989871db
Bobbie Chen <bobbiechen75@gmail.com>
2018-02-09 21:40:01 -0500

    Initial skeleton

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
+@app.route('/report-flush', methods=['GET', 'POST'])
+def report_flush():
+    if request.method == 'GET':
+        return "Get not supported on this page"
+        
+    # Todo: write to the database
+    return "Post success", 200
